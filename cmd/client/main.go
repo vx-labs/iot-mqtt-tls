88ebd6ae3fc45da59a250d7a7322a3ec0f9478f0
Julien BONACHERA <jbonachera@xebia.fr>
2018-01-28 21:35:44 +0100

    adapt cli to api change

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -14,10 +14,9 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	cert, _, err := c.GetCertificate(context.Background(), "test")
+	cert, err := c.GetCertificate(context.Background(), "test")
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	fmt.Println(cert.Subject.CommonName)
-	pem.Encode(os.Stdout, &pem.Block{Bytes: cert.Raw, Type: "CERTIFICATE"})
+	pem.Encode(os.Stdout, &pem.Block{Bytes: cert[0].Leaf.Raw, Type: "CERTIFICATE"})
 }
